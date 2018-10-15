d829d1717919e0353293f8e556824046eee36d9f
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-10-15 10:56:39 +0200

    init

---
+++ src/main/java/eaiproject/eaiprojectPayment/EaiprojectPaymentApplication.java
@@ -0,0 +1,12 @@
+package eaiproject.eaiprojectPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiprojectPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiprojectPaymentApplication.class, args);
+	}
+}
